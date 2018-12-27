bd288c6868963eed613d6af20939a94c12612384
pupali <40403483+pupali@users.noreply.github.com>
2018-12-27 13:30:58 +0300

    added hls player component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,10 +23,10 @@ import { PlayerComponent } from './player/player.component';
 
 // Videogular 2 Modules
 import {VgCoreModule} from 'videogular2/core';
-import {VgControlsModule} from 'videogular2/controls';
-import {VgOverlayPlayModule} from 'videogular2/overlay-play';
-import {VgBufferingModule} from 'videogular2/buffering';
-import { VgStreamingModule } from 'videogular2/streaming';
+import {VgControlsModule} from '../../videogular2/controls';
+import {VgOverlayPlayModule} from '../../videogular2/overlay-play';
+import {VgBufferingModule} from '../../videogular2/buffering';
+import { VgStreamingModule } from '../../videogular2/streaming';
 
 
 
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BitrateOption, VgAPI } from '../../../videogular2/core';
 
 @Component({
   selector: 'app-player',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player.component.scss']
 })
 export class PlayerComponent implements OnInit {
-
+  videoBitrates: BitrateOption[];
+  videoApi: VgAPI;
   constructor() { }
 
   ngOnInit() {
   }
+
+  setBitrates(recivedBitrates: BitrateOption[]) {
+    recivedBitrates[1].label = 'SD';
+    recivedBitrates[2].label = 'HD';
+    this.videoBitrates = recivedBitrates;
+  }
+
+  onPlayerInit(api: VgAPI) {
+    this.videoApi = api;
+}
 
 }
