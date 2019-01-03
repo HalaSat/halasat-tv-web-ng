bf84455e678ef102faaf7a510a95448e3928495a
Mohammed Salman <msal4@outlook.com>
2019-01-03 08:07:39 +0300

    mobile responsive

--- src/app/channel-player/channel-player.component.ts
+++ src/app/channel-player/channel-player.component.ts
@@ -1,35 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
-import { Channel } from '../core/channel.model';
-import { ChannelService } from '../channel.service';
+import { Channel } from "../core/channel.model";
+import { ChannelService } from "../channel.service";
 
 @Component({
-  selector: 'app-channel-player',
-  templateUrl: './channel-player.component.html',
-  styleUrls: ['./channel-player.component.scss'],
+  selector: "app-channel-player",
+  templateUrl: "./channel-player.component.html",
+  styleUrls: ["./channel-player.component.scss"]
 })
 export class ChannelPlayerComponent implements OnInit {
   channels: Channel[];
   channel: Channel;
   constructor(
     private route: ActivatedRoute,
-    private channelService: ChannelService,
-  ) { }
+    private channelService: ChannelService
+  ) {}
 
   ngOnInit() {
     this.channelService.getChannels().subscribe(channels => {
       this.channels = channels;
     });
-    this.route.params.subscribe( (params) => {
-      this.setChannel(params['id']);
+    this.route.params.subscribe(params => {
+      this.setChannel(params["id"]);
     });
-
   }
-  setChannel( id) {
-   this.channel = this.channels.find((channel) => {
-     return channel.id == id;
-   }
-   );
+  setChannel(id) {
+    this.channel = this.channels.find(channel => channel.id == id);
   }
 }
