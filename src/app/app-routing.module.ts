27f757e8066729d5835254d4770c204137401809
Mohammed Salman <msal4@outlook.com>
2019-01-06 11:24:47 +0300

    mobile support and various changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { ChannelCardComponent } from './channel-card/channel-card.component';
-import { ChannelsComponent } from './channels/channels.component';
-import { ChannelPlayerComponent } from './channel-player/channel-player.component';
+import { ChannelCardComponent } from "./channel-card/channel-card.component";
+import { ChannelsComponent } from "./channels/channels.component";
+import { ChannelPlayerComponent } from "./channel-player/channel-player.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'channels', pathMatch: 'full' },
-  { path: 'channel-card', component: ChannelCardComponent },
-  { path: 'channels', component: ChannelsComponent },
-  { path: 'play/:id', component: ChannelPlayerComponent,},
+  { path: "channel-card", component: ChannelCardComponent },
+  { path: "", component: ChannelsComponent },
+  { path: "play/:id", component: ChannelPlayerComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes) ],
-  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
